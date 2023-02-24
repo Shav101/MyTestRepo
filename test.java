c3336f1a3e8b5bb86cf5cc80ccbb0cea8d76d0ec
Shav Djumaev <sj192710@gmail.com>
2023-02-23 21:39:45 -0500

    Finished my work

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class test{
 	public static void main (String [] args){
-		System.out.println("Changes From SDET-2");
+		System.out.println("Changes From DEV1");
+		System.out.println("More changes from dev1");
 	}
 
 }
